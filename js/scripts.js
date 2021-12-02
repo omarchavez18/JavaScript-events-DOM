8870935118a9f7063f46e8dc50eb4b4e8fa903dd
Omar <omarchavezgutierrezdevelasco@gmail.com>
2021-12-01 21:18:21 -0600

    JavaScript events DOM

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// Para seleccionar contenido hay 3 formas.
+//querySelector
+const heading = document.querySelector(".header__texto h2"); //retorna 0 o 1 elementos.
+console.log(heading);
+
+//querySelectorAll
+
+//getElementById
