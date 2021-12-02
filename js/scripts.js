3ad4ac0e79eb521da96c82ba65295aeb3ba936e7
Omar <omarchavezgutierrezdevelasco@gmail.com>
2021-12-01 21:42:51 -0600

    queryselector

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 // Para seleccionar contenido hay 3 formas.
-//querySelector
-const heading = document.querySelector(".header__texto h2"); //retorna 0 o 1 elementos.
+//querySelector //retorna 0 o 1 elementos.
+const heading = document.querySelector(".header__texto h2");
+heading.textContent = "Nuevo Heading";
+heading.classList.add("nueva-clase");
 console.log(heading);
 
 //querySelectorAll
