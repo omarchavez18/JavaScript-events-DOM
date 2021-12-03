0249d9b69f3143e7e9c1eeaf9290d8e068ead197
Omar <omarchavezgutierrezdevelasco@gmail.com>
2021-12-02 19:43:08 -0600

    queryselector & createElement()

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,29 @@ console.log(heading);
 const enlaces = document.querySelectorAll(".navegacion a");
 enlaces[0].textContent = "nuevo texto para enlace";
 enlaces[0].classList.add("nueva-clase");
+
+//Generar un nuevo enlace
+// al crear algo de html siempre inicia con 'document', lo que se desea crear debe de ir en mayusculas.
+const nuevoEnlace = document.createElement("A");
+console.log(nuevoEnlace);
+
+//en consola solo aparece la etiqueta <a></a> por lo que le falta la direccion href el texto etc..
+// y esto tenemos que agregarlo
+
+//agregar el href.
+nuevoEnlace.href = "nuevo-enlace.html";
+
+//agregar el texto.
+nuevoEnlace.textContent = "Tienda virtual";
+
+//agregar la clase.
+nuevoEnlace.classList.add("navegacion__enlace");
+
+//falta agregarlo al documento. y hay que indicarle al JS donde queremos agregarlo.
+//lo agregaremos a la seccion inicial de navegacion.
+const navegacion = document.querySelector(".navegacion");
+
+//.appendChild es una funcion que nos permite agregar un elemento e insertarlo en otro lugar.
+navegacion.appendChild(nuevoEnlace);
+
+console.log(nuevoEnlace);
